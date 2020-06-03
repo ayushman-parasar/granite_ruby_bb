34ba6d751fb3a9652c072ab0ffafd06f1a74d002
Ayush_Mann <learnertsk@gmail.com>
2020-06-03 17:53:42 +0530

    added react component for displaying lists

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    render
+    @tasks = Task.all
   end
 
   # GET /tasks/1
---
+++ app/javascript/components/tasks/List.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class List extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <p className="text-primary"> Task list will be appearing here soon !!! </p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default List
