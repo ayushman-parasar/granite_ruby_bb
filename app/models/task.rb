4fa00b5c0f1d2ef9a84f8b4c310f7c068b8cb29b
Ayush_Mann <learnertsk@gmail.com>
2020-06-09 17:27:40 +0530

    removed setup from task_test

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   validates :desc, presence: true
-  belongs_to :user, optional: :true
+  belongs_to :user, optional: :true, foreign_key: "user_id"
   has_many :comments, dependent: :destroy
   # before_validation :set_description
   # after_validation :set_description
