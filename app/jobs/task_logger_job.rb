cd63fb4a278a2cd183aa4b2a40ef18038f27d740
Ayush_Mann <learnertsk@gmail.com>
2020-06-12 17:51:36 +0530

    included redis in the docker

--- app/jobs/task_logger_job.rb
+++ app/jobs/task_logger_job.rb
@@ -1,19 +1,23 @@
 class TaskLoggerJob < ApplicationJob
   # queue_as :default
 
-  # before_enqueue :print_before_enqueue_message
-  # after_enqueue :print_after_enqueue_message
+  before_enqueue :print_before_enqueue_message
+  after_enqueue :print_after_enqueue_message
+
+  # def perform
+  #   puts "TaskLoggerJob is performed"
+  # end
 
   def perform(task)
     puts "Created a task with following attributes :: #{task.attributes} "
   end
 
-  # def print_before_enqueue_message
-  #   puts "Printing from inside before_enqueue callback"
-  # end
+  def print_before_enqueue_message
+    puts "Printing from inside before_enqueue callback"
+  end
 
-  # def print_after_enqueue_message
-  #   puts "Printing from inside after_enqueue callback"
-  # end
+  def print_after_enqueue_message
+    puts "Printing from inside after_enqueue callback"
+  end
 
 end
